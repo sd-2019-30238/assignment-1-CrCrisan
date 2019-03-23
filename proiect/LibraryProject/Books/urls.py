a41f09eebe20adf6d8b2f25f70cc32bacbd49205
Crisan Cristian <cristicrisan8@gmail.com>
2019-03-23 23:49:03 +0200

    Add book detail page

--- proiect/LibraryProject/Books/urls.py
+++ proiect/LibraryProject/Books/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'Book'
+
 urlpatterns = [
-    path('', views.bookList),
+    path('', views.bookList, name = 'list'),
+    path('<str:slug>', views.bookDetail, name = 'details'),
 ]
--- proiect/LibraryProject/Books/views.py
+++ proiect/LibraryProject/Books/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . import models
+from .models import Book
 
 def bookList(request):
-    books = models.Book.objects.all().order_by('title')
+    books = Book.objects.all().order_by('title')
     return render(request, 'Books/BooksList.html', {'books' : books})
+
+def bookDetail(request, slug):
+    book = Book.objects.get(slug = slug)
+    return render(request, 'Books/BookDetail.html', {'book' : book})
